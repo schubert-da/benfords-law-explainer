eda3de97cfb36b623c77cef0a5a4e511d7827c12
schubert-da <schubertdeabreu2@gmail.com>
2025-09-14 19:33:27 +0530

    adds katex support

---
+++ src/lib/components/FormulaBox.svelte
@@ -0,0 +1,19 @@
+<script>
+	import katex from 'katex';
+	import 'katex/dist/katex.min.css';
+	import { onMount } from 'svelte';
+
+	export let formula = null;
+
+	let container;
+	onMount(() => {
+		katex.render(formula, container, {
+			throwOnError: false,
+			displayMode: false
+		});
+	});
+</script>
+
+{#if formula}
+	<span bind:this={container}></span>
+{/if}
