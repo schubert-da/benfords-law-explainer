e1d9c760282fe3993965be51f346b6d120d71916
schubert-da <schubertdeabreu2@gmail.com>
2025-09-14 19:33:19 +0530

    page and layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../app.css';
+	import '../styles/app.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
@@ -9,4 +9,11 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<div class="opening-scrolly">
+	<div class="placeholder h-100 !w-screen !max-w-none">
+		<h1>Benford's Law Explainer</h1>
+	</div>
+</div>
+<main>
+	{@render children?.()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import FormulaBox from '$lib/components/FormulaBox.svelte';
+</script>
+
+<section class="title-section">
+	<h2>Benford's Law Explainer</h2>
+	<p>An exploration of how real-world data often deviates from randomness.</p>
+</section>
+
+<section class="introduction">
+	<p>
+		So what is Benford's Law and how does it magically govern the distributions of datasets in the
+		wild? The basic principle of this law is that the leading digit of numbers in a dataset are more
+		frequently smaller going to be small.
+	</p>
+
+	<p>“How small”, you ask? Well the math behind the exact probability of a leading digit is:</p>
+
+	<FormulaBox formula="P(d) = \\log_{10}(d + 1) - \\log_{10}(d)"></FormulaBox>
+</section>
