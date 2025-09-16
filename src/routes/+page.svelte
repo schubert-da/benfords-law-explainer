f4fe49fc30d2e4090f22e8d403b9d49d6f452de8
schubert-da <schubertdeabreu2@gmail.com>
2025-09-16 10:50:58 +0530

    adds new copy sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section class="title-section">
-	<h2>Benford's Law Explainer</h2>
+	<h2 class="text-4xl font-bold">Benford's Law Explainer</h2>
 	<p>An exploration of how real-world data often deviates from randomness.</p>
 </section>
 
@@ -17,4 +17,24 @@
 	<p>“How small”, you ask? Well the math behind the exact probability of a leading digit is:</p>
 
 	<FormulaBox formula="P(d) = \\log_{10}(d + 1) - \\log_{10}(d)"></FormulaBox>
+
+	<p>
+		where d is the digit we want to find the probability of. So essentially this boils down to the
+		difference between the logarithm of the next digit and that of the current digit.
+	</p>
+
+	<p>When graphed, this formula produces the following distribution:</p>
+
+	<div class="placeholder h-100">Graph of Benford's Law Distribution Here</div>
+
+	<p>
+		In an even distribution we would expect each to have a probability of 11.11%, but in practice
+		Benford's law puts the odds of a leading digit of 1 to be 30.1% - almost three times more
+		likely! Additionally, you are more than 6 times more likely to find a digit with a leading digit
+		of 1 than a 9!
+	</p>
+</section>
+
+<section class="deep-dive">
+	<h2 class="text-4xl font-bold">The more interesting ones</h2>
 </section>
