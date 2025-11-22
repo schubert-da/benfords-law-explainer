187880b7f64f84351fcdc11f952899bbb7221146
schubert-da <schubertdeabreu2@gmail.com>
2025-11-22 21:48:11 +0530

    Adds components page and proportion plot placeholders

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,12 +9,14 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<!-- 
 <div class="opening-scrolly">
 	<div class="placeholder h-100 flex !w-screen !max-w-none flex-col items-center justify-center">
 		<h1>Benford's Law Explainer</h1>
 		<p>Opening Scrolly</p>
 	</div>
 </div>
+ -->
 <main>
 	{@render children?.()}
 </main>
---
+++ src/routes/proportion-plots/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import columnData from '$assets/data/columns_overview.json';
+	import ProportionPlot from '$components/charts/ProportionPlot/ProportionPlot.svelte';
+
+	// console.log(columnData);
+</script>
+
+<section class="components-grid">
+	<h1 class="text-4xl">Page with all the Proportion Plots</h1>
+
+	<div class="plot-grid">
+		{#each columnData.slice(0, 1) as column}
+			<div class="proportion-plot-container">
+				<ProportionPlot data={column}></ProportionPlot>
+			</div>
+		{/each}
+	</div>
+</section>
