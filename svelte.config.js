54774ae2963eefbb5322183b762574b895a30d13
schubert-da <schubertdeabreu2@gmail.com>
2025-11-22 21:47:19 +0530

    add aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+    kit: {
+        adapter: adapter(),
+        alias: {
+            $components: 'src/lib/components',
+            $assets: 'src/lib/assets',
+            $utils: 'src/lib/utils'
+        }
+    }
+};
 
 export default config;
