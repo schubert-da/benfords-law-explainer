3c54ced6adff7defc03df065011a6a0c22a2fbb7
schubert-da <schubertdeabreu2@gmail.com>
2025-11-27 22:54:45 +0530

    adds barcharts test page

---
+++ src/routes/bar-charts/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import columnData from '$assets/data/columns_overview.json';
+	import ChartContainer from '$components/charts/ChartContainer.svelte';
+
+	// console.log(columnData);
+</script>
+
+<section class="components-grid !max-w-[90vw]">
+	<h1 class="text-4xl">Page with all the Proportion Plots</h1>
+
+	<div class="plot-grid flex flex-row flex-wrap items-stretch justify-center gap-4">
+		{#each columnData.slice() as column}
+			<div class="proportion-plot-container">
+				<ChartContainer data={column} chartType="barchart"></ChartContainer>
+			</div>
+		{/each}
+	</div>
+</section>
