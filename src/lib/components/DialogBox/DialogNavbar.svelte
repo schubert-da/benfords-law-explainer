3ba4246cd862aa9eb0944b296ded4e58b7e4b10e
schubert-da <schubertdeabreu2@gmail.com>
2025-12-04 19:36:08 +0530

    adds chart toggles

--- src/lib/components/DialogBox/DialogNavbar.svelte
+++ src/lib/components/DialogBox/DialogNavbar.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { scrollerState } from '$utils/stores';
+	import { scrollerState, chosenChart } from '$utils/stores';
 	import toggleBarchartImage from '$assets/image/toggle-barchart.jpg';
 	import toggleProportionImage from '$assets/image/toggle-proportions.jpg';
 
@@ -28,17 +28,17 @@
 				class="chart-type-picker flex items-center justify-center gap-1.5 rounded border-[1px] border-[#888] p-1 pt-1.5 sm:p-1.5 sm:pt-2"
 			>
 				<button
-					class="chart-type-button w-15 h-15 relative overflow-hidden rounded border-[1px] border-[#888] bg-gray-400 text-base md:h-20 md:w-20"
-					class:selected={selectedChartType === 'bar'}
-					on:click={() => (selectedChartType = 'bar')}
+					class="chart-type-button hover:border-scale-diverging-0 w-15 h-15 relative cursor-pointer overflow-hidden rounded border-[1px] border-[#888] bg-gray-400 text-base hover:border-2 md:h-20 md:w-20"
+					class:selected={$chosenChart === 'barchart'}
+					on:click={() => ($chosenChart = 'barchart')}
 				>
 					<img src={toggleBarchartImage} alt="Bar Chart" />
 				</button>
 
 				<button
-					class="chart-type-button w-15 h-15 relative overflow-hidden rounded border-[1px] border-[#888] bg-gray-400 text-base md:h-20 md:w-20"
-					class:selected={selectedChartType === 'proportion'}
-					on:click={() => (selectedChartType = 'proportion')}
+					class="chart-type-button hover:border-scale-diverging-0 w-15 h-15 relative cursor-pointer overflow-hidden rounded border-[1px] border-[#888] bg-gray-400 text-base hover:border-2 md:h-20 md:w-20"
+					class:selected={$chosenChart === 'proportion'}
+					on:click={() => ($chosenChart = 'proportion')}
 				>
 					<img src={toggleProportionImage} alt="Proportion Chart" />
 				</button>
@@ -70,5 +70,10 @@
 		height: 100%;
 		pointer-events: none;
 		box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.2);
+	}
+
+	button:not(.selected) {
+		opacity: 0.6;
+		filter: grayscale(0.5);
 	}
 </style>
