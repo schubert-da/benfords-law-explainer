2e1faaa34c66b064ff55c0b0bda9359714d75f92
schubert-da <schubertdeabreu2@gmail.com>
2025-12-04 11:58:23 +0530

    section title responsiveness

--- src/lib/components/common/SectionTitle.svelte
+++ src/lib/components/common/SectionTitle.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="section-title flex w-full flex-col items-center gap-2 text-center">
-	<h2 class="text-[42px] font-bold">{title}</h2>
+	<h2 class="text-[32px] font-bold leading-snug md:text-[42px]">{title}</h2>
 	{#if subtitle}
 		<p class="max-w-[30ch] text-balance">{subtitle}</p>
 	{/if}
